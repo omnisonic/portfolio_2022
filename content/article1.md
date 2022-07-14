8034e58652407bf18d1850e10b76255037f9dd46
omnisonic <omnisonic@gmail.com>
2022-07-13 18:01:12 -0700

    added repos

--- content/article1.md
+++ content/article1.md
@@ -1,4 +1,36 @@
 Title: page1
 date: 2022-09-13
 
-Hello World this is an article
+Hello World these are my repos
+
+https://github.com/omnisonic/album_landing_page.git
+https://github.com/omnisonic/currency_chart.git
+https://github.com/omnisonic/flashcard.git
+https://github.com/omnisonic/gas_miles.git
+https://github.com/omnisonic/gh_test.git
+https://github.com/omnisonic/gtypist_create.git
+https://github.com/omnisonic/hello-world.git
+https://github.com/omnisonic/JavaScriptChart.git
+https://github.com/omnisonic/jcm_pelican.git
+https://github.com/omnisonic/jctech-pelican.git
+https://github.com/omnisonic/jctech_cheatsheets.git
+https://github.com/omnisonic/jctech_django.git
+https://github.com/omnisonic/joe-coach.git
+https://github.com/omnisonic/massively_theme.git
+https://github.com/omnisonic/momsite.git
+https://github.com/omnisonic/pelican-simple-theme.git
+https://github.com/omnisonic/portfolio_2022.git
+https://github.com/omnisonic/Python_Study.git
+https://github.com/omnisonic/raspberrypi_hq_av_live_streamer.git
+https://github.com/omnisonic/react_chart.git
+https://github.com/omnisonic/react_chart_componentized.git
+https://github.com/omnisonic/socratica-python.git
+https://github.com/omnisonic/sonic-pi.git
+https://github.com/omnisonic/Static-Website-Build-Python.git
+https://github.com/omnisonic/studious-parakeet.git
+https://github.com/omnisonic/theme_demo.git
+https://github.com/omnisonic/vanomad.git
+https://github.com/omnisonic/website-templates-jcteched.git
+https://github.com/omnisonic/worklog.git
+https://github.com/omnisonic/work_log.git
+https://github.com/omnisonic/youtube-api-to-blog-post.git
--- githubapi.py
+++ githubapi.py
@@ -1,8 +1,70 @@
 # get list of repositories for a user from github
-def get_repos(username):
-    import requests
-    url = 'https://api.github.com/users/' + username + '/repos'
-    r = requests.get(url)
-    return r.json()
+# def get_repos(username):
+#     import requests
+#     url = 'https://api.github.com/users/' + username + '/repos'
+#     r = requests.get(url)
+#     return r.json()
 
-# get links to repositories for a user
+# json_data = get_repos('omnisonic')
+
+# #  output json to html
+# def json_to_html(json_data):
+#     import json
+#     import html
+#     html_data = html.escape(json.dumps(json_data, indent=4))
+#     return html_data
+#     #save to file
+
+# html_data = json_to_html(json_data)
+# with open('output.html', 'w') as f:
+#     f.write(html_data)
+    
+
+
+#!/usr/bin/env python3.9
+#-*- coding: utf-8 -*-
+import readline, sys, os, requests
+
+import click
+from github import Github
+
+g = Github()
+username = 'omnisonic'
+
+def userexists(username):
+    addr = "https://api.github.com/users/" + username
+    response = requests.get(addr)
+    if response.status_code == 404:
+        return False
+    else:
+        if response.status_code == 200:
+            return True
+
+def printrepos(repos):
+        original_repos = []
+        for repo in repos:
+            if repo.fork is False and repo.archived is False:
+                print(repo.clone_url)
+ 
+            #format repo as html link
+                html_data = '<a href="' + repo.clone_url + '">' + repo.clone_url + '</a><br>' + '\n'
+
+                with open('output.html', 'a') as f:
+                    f.write(html_data)
+
+@click.command()
+@click.argument('username')
+def main(username):
+    if userexists(username):
+        user = g.get_user(username)
+        repos = user.get_repos()
+        printrepos(repos)
+        # write file with html link of all repos
+
+
+    else:
+        print("Username doesn't exist")
+
+
+if __name__ == "__main__":
+    main()
