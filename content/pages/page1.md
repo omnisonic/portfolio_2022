c1090784a67755076de6c086977d5b40293dce14
omnisonic <omnisonic@gmail.com>
2022-07-15 13:28:01 -0700

    one page template integrated

--- content/pages/page1.md
+++ content/pages/page1.md
@@ -2,34 +2,181 @@ Title: Welcome
 URL:
 save_as: index.html
 
-<a href="https://github.com/omnisonic/album_landing_page.git">https://github.com/omnisonic/album_landing_page.git</a><br>
-<a href="https://github.com/omnisonic/currency_chart.git">https://github.com/omnisonic/currency_chart.git</a><br>
-<a href="https://github.com/omnisonic/flashcard.git">https://github.com/omnisonic/flashcard.git</a><br>
-<a href="https://github.com/omnisonic/gas_miles.git">https://github.com/omnisonic/gas_miles.git</a><br>
-<a href="https://github.com/omnisonic/gh_test.git">https://github.com/omnisonic/gh_test.git</a><br>
-<a href="https://github.com/omnisonic/gtypist_create.git">https://github.com/omnisonic/gtypist_create.git</a><br>
-<a href="https://github.com/omnisonic/hello-world.git">https://github.com/omnisonic/hello-world.git</a><br>
-<a href="https://github.com/omnisonic/JavaScriptChart.git">https://github.com/omnisonic/JavaScriptChart.git</a><br>
-<a href="https://github.com/omnisonic/jcm_pelican.git">https://github.com/omnisonic/jcm_pelican.git</a><br>
-<a href="https://github.com/omnisonic/jctech-pelican.git">https://github.com/omnisonic/jctech-pelican.git</a><br>
-<a href="https://github.com/omnisonic/jctech_cheatsheets.git">https://github.com/omnisonic/jctech_cheatsheets.git</a><br>
-<a href="https://github.com/omnisonic/jctech_django.git">https://github.com/omnisonic/jctech_django.git</a><br>
-<a href="https://github.com/omnisonic/joe-coach.git">https://github.com/omnisonic/joe-coach.git</a><br>
-<a href="https://github.com/omnisonic/massively_theme.git">https://github.com/omnisonic/massively_theme.git</a><br>
-<a href="https://github.com/omnisonic/momsite.git">https://github.com/omnisonic/momsite.git</a><br>
-<a href="https://github.com/omnisonic/pelican-simple-theme.git">https://github.com/omnisonic/pelican-simple-theme.git</a><br>
-<a href="https://github.com/omnisonic/portfolio_2022.git">https://github.com/omnisonic/portfolio_2022.git</a><br>
-<a href="https://github.com/omnisonic/Python_Study.git">https://github.com/omnisonic/Python_Study.git</a><br>
-<a href="https://github.com/omnisonic/raspberrypi_hq_av_live_streamer.git">https://github.com/omnisonic/raspberrypi_hq_av_live_streamer.git</a><br>
-<a href="https://github.com/omnisonic/react_chart.git">https://github.com/omnisonic/react_chart.git</a><br>
-<a href="https://github.com/omnisonic/react_chart_componentized.git">https://github.com/omnisonic/react_chart_componentized.git</a><br>
-<a href="https://github.com/omnisonic/socratica-python.git">https://github.com/omnisonic/socratica-python.git</a><br>
-<a href="https://github.com/omnisonic/sonic-pi.git">https://github.com/omnisonic/sonic-pi.git</a><br>
-<a href="https://github.com/omnisonic/Static-Website-Build-Python.git">https://github.com/omnisonic/Static-Website-Build-Python.git</a><br>
-<a href="https://github.com/omnisonic/studious-parakeet.git">https://github.com/omnisonic/studious-parakeet.git</a><br>
-<a href="https://github.com/omnisonic/theme_demo.git">https://github.com/omnisonic/theme_demo.git</a><br>
-<a href="https://github.com/omnisonic/vanomad.git">https://github.com/omnisonic/vanomad.git</a><br>
-<a href="https://github.com/omnisonic/website-templates-jcteched.git">https://github.com/omnisonic/website-templates-jcteched.git</a><br>
-<a href="https://github.com/omnisonic/worklog.git">https://github.com/omnisonic/worklog.git</a><br>
-<a href="https://github.com/omnisonic/work_log.git">https://github.com/omnisonic/work_log.git</a><br>
-<a href="https://github.com/omnisonic/youtube-api-to-blog-post.git">https://github.com/omnisonic/youtube-api-to-blog-post.git</a><br>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+    dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "G-MY1T6NLGD0");
+</script>
+
+<link rel="stylesheet" href="/theme/css/style_copy.css" />
+<link
+    href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic"
+    rel="stylesheet"
+/>
+<link
+    href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
+    rel="stylesheet"
+/>
+<link
+    href="https://fonts.googleapis.com/css?family=Crete+Round:regular,italic"
+    rel="stylesheet"
+/>
+<script
+    src="https://kit.fontawesome.com/e971436820.js"
+    crossorigin="anonymous"
+></script>
+
+<section class="main_page">
+<section class="recent_works">
+    <div class="works_title">
+        <h3>My Recent Works</h3>
+        <hr />
+    </div>
+    <div class="headlines">
+        <div class="image_headline">
+        <img src="./images/To-do.png" alt="Image place holder" />
+        </div>
+        <div class="discription_headline">
+        <h4>Minimalist to-do list project</h4>
+        <p>
+            In this minimalist to do list project I have used the best practice of java script, HTML and CSS. This project is fully responsive both for desktop and also for mobile version. Java script has been used to populate the HTML elements and CSS has been used to give the website a better look. As a measurement unit I have used rem and em to make it more adaptive to different vendors
+        </p>
+        <ul class="tags">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>Webpack</li>
+        </ul>
+        <button class="popup_display_button" id="1" type="button">See Project</button>
+        </div>
+    </div>
+    
+</section>
+<section class="grid-container" id="Portfolio">
+</section>
+<section class="about-me" id="About">
+<div class="description_about_my_self">
+    <h3>About Me</h3>
+    <p>
+    Hello I’m a software developer! I can help you build a product ,
+    feature or website Look through some of my work and experience! If you
+    like what you see and have a project you need coded, don’t hestiate to
+    contact me.
+    </p>
+    <div class="image-container">
+    <button type="button">Get My Resume</button>
+    </div>
+    <hr />
+</div>
+<div class="language_frameworks_and_skills">
+    <div class="languag_skillset">
+    <ul>
+        <li class="language">Language</li>
+        <li class="my_skills">Javascript</li>
+        <li class="my_skills">Tuby</li>
+        <li class="my_skills">Html</li>
+        <li class="my_skills">Css</li>
+    </ul>
+    </div>
+    <div class="frameworks_skillset">
+    <ul>
+        <li class="frameworks">Frameworks</li>
+        <li class="my_skills">Bootstrap</li>
+        <li class="my_skills">Ruby on Rails</li>
+        <li class="my_skills">RSPec</li>
+        <li class="my_skills">Capy Bara</li>
+        <li class="my_skills">Solenium</li>
+    </ul>
+    </div>
+    <div class="skills_skillset">
+    <ul>
+        <li class="skills">Skills</li>
+        <li class="my_skills">Codekit</li>
+        <li class="my_skills">Github</li>
+        <li class="my_skills">Codepen</li>
+        <li class="my_skills">Gitlab</li>
+        <li class="my_skills">Terminal</li>
+    </ul>
+    </div>
+</div>
+</section>
+<section class="motivation_and_form" id="Contact">
+<div class="motivation">
+    <p>
+    I'm always interested in hearing about new projects, so if you'd like
+    to chat please get in touch.
+    </p>
+</div>
+<div class="form">
+    <form action="https://formspree.io/f/xeqnejbr" class ="contact_form" method="post">
+    <ul class="form-inputs">
+        <li>
+        <label for="full_name"></label>
+        <input
+            type="text"
+            id="full_name"
+            placeholder="Full name"
+            name="Full_name"
+            maxlength="30"
+            required
+        />
+        </li>
+        <li>
+        <label for="email"></label>
+        <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            name="Email address"
+            required
+        />
+        <small class="small"></small>
+        </li>
+        <li>
+        <label for="comment"></label>
+        <textarea
+            id="comment"
+            placeholder="Enter text here"
+            name="Comment"
+            maxlength="500"
+            required
+        ></textarea>
+        </li>
+    </ul>
+    <div class="comment_button">
+        <button id="submit_comment" type="submit">Get in touch</button>
+    </div>
+    </form>
+</div>
+</section>
+<section class="footer">
+<div class="rectangle">
+    <hr class="footer_line" />
+</div>
+<ul class="socials_bottom">
+    <li>
+    <a href=""><i class="fab fa-github"></i></a>
+    </li>
+    <li>
+    <a href=""><i class="fab fa-linkedin-in"></i></a>
+    </li>
+    <li>
+    <a href=""><i class="fab fa-angellist"></i></a>
+    </li>
+    <li>
+    <a href=""><i class="fab fa-twitter"></i></a>
+    </li>
+    <li>
+    <a href=""><i class="fab fa-medium-m"></i></a>
+    </li>
+</ul>
+<hr class="bottom_footer_line" />
+</section>
+</section>
+<section class="pop_up_window" id="pop_up_container">      
+</section> 
+<script src="/theme/main.js"></script>
---
+++ simple-jctech/static/main.js
@@ -0,0 +1,223 @@
+// // const hamburger = document.getElementById('hamburger');
+// const menuItems = document.querySelector('.menu_items');
+// const mobileMenu = document.querySelector('.mobile_menu');
+// const closeButton = document.querySelector('.close_button');
+// const gotoItems = document.querySelectorAll('.goto');
+// function toggling() {
+//   menuItems.classList.toggle('appear');
+//   mobileMenu.classList.toggle('open');
+// }
+// // hamburger.addEventListener('click', toggling);
+// closeButton.addEventListener('click', toggling);
+// for (let i = 0; i < gotoItems.length; i += 1) {
+//   gotoItems[i].onclick = toggling;
+// }
+
+/* Populating the portfolio */
+const projects = [
+  {
+    title: 'Minimalist to-do list project',
+    languages: ['HTML and CSS', 'Javascript', 'Webpack'],
+    imageUrl: './images/To-do.png',
+    body: "In this minimalist to do list project I have used the best practice of java script, HTML and CSS. This project is fully responsive both for desktop and also for mobile version. Java script has been used to populate the HTML elements and CSS has been used to give the website a better look. As a measurement unit I have used rem and em to make it more adaptive to different vendors",
+    desc: "This is an easy responsive to-do list web application. You can write your tasks, edit, check completed or delete tasks. It is completely responsive and easy to use website application",
+    id: 1,
+    seeLiveUrl: 'https://micky373.github.io/To_DO_List/dist/',
+    seeSourceUrl: 'https://github.com/Micky373/To_DO_List',
+  },
+  {
+    title: 'Professional Art Printing Data',
+    languages: ['html', 'Bootstrap', 'Ruby'],
+    imageUrl: './images/portfolio_image.svg',
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    id: 2,
+    seeLiveUrl: 'https://github.com/omnisonic',
+    seeSourceUrl:'https://github.com/omnisonic',
+  },
+  {
+    title: 'Professional Art Printing Data',
+    languages: ['html', 'Bootstrap', 'Ruby'],
+    imageUrl: './images/portfolio_image.svg',
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    id: 3,
+    seeLiveUrl: 'https://github.com/omnisonic',
+    seeSourceUrl:'https://github.com/omnisonic',
+  },
+  {
+    title: 'Professional Art Printing Data',
+    languages: ['html', 'Bootstrap', 'Ruby'],
+    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    imageUrl: './images/portfolio_image.svg',
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    id: 4,
+    seeLiveUrl: 'https://github.com/omnisonic',
+    seeSourceUrl:'https://github.com/omnisonic',
+  },
+  {
+    title: 'Professional Art Printing Data',
+    languages: ['html', 'Bootstrap', 'Ruby'],
+    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    imageUrl: './images/portfolio_image.svg',
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    id: 5,
+    seeLiveUrl: 'https://github.com/omnisonic',
+    seeSourceUrl:'https://github.com/omnisonic',
+  },
+  {
+    title: 'Professional Art Printing Data',
+    languages: ['html', 'Bootstrap', 'Ruby'],
+    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    imageUrl: './images/portfolio_image.svg',
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    id: 6,
+    seeLiveUrl: 'https://github.com/omnisonic',
+    seeSourceUrl:'https://github.com/omnisonic',
+  },
+  {
+    title: 'Professional Art Printing Data',
+    languages: ['html', 'Bootstrap', 'Ruby'],
+    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    imageUrl: './images/portfolio_image.svg',
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    id: 7,
+    seeLiveUrl: 'https://github.com/omnisonic',
+    seeSourceUrl:'https://github.com/omnisonic',
+  },
+];
+
+const portfolio = document.getElementById('Portfolio');
+let template = '';
+for (let i = 1; i < projects.length; i += 1) {
+  template += `
+  <div class="grid-item">
+      <div class="grid-item-headline">
+          <h3>${projects[i].title}</h3>
+          <p>${projects[i].desc}</p>
+      </div>
+      <ul class="tags">
+              <li>${projects[i].languages[0]}</li>
+              <li>${projects[i].languages[1]}</li>
+              <li>${projects[i].languages[2]}</li>
+      </ul>
+      <button class="popup_display_button" id="${projects[i].id}" type="button">See Project</button>
+  </div>`;
+}
+
+portfolio.innerHTML = template;
+
+// Populating the pop up modal window
+
+let popUpTemplate = '';
+for (let i = 0; i < projects.length; i += 1) {
+  popUpTemplate += `
+        <div class="pop_up" id="${projects[i].id}">
+            <div class="pop_up_content">
+                <h3>${projects[i].title}</h3>
+                <div class="close_button">
+                    <img class="close_modal" src="/theme/images/close_btn.svg" alt="Closing pop up menu button" />
+                </div>
+            </div>
+            <ul class="pop_up_tags">
+                <li>${projects[i].languages[0]}</li>
+                <li>${projects[i].languages[1]}</li>
+                <li>${projects[i].languages[2]}</li>
+            </ul>
+            <div class="pop_up_image_and_content">
+                <div class="image_content">
+                    <img id="pop_up_image" src="${projects[i].imageUrl}" alt="Image place holder for multi post stories" />
+                </div>
+                <div class="descriptive_content">
+                    <div>
+                        <p>${projects[i].body}</p>
+                    </div>
+                    <div class="pop_up_links">
+                        <a href='${projects[i].seeLiveUrl}'><button type="button">See Live&nbsp;&nbsp;<i class="fa fa-signal" aria-hidden="true"></i></button></a>
+                        <a href='${projects[i].seeSourceUrl}'><button type="button">See Source&nbsp;&nbsp;<i id="github-icon-pop_up" class="fab fa-github"></i></button></a>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+}
+const popUpContainer = document.getElementById('pop_up_container');
+popUpContainer.innerHTML = popUpTemplate;
+
+// Popup window functionality
+
+const popUpButtons = document.querySelectorAll('.popup_display_button');
+const closeModals = document.querySelectorAll('.close_modal');
+const mainPage = document.querySelector('.main_page');
+const popUps = document.querySelectorAll('.pop_up');
+for (let i = 0; i < popUpButtons.length; i += 1) {
+  popUpButtons[i].addEventListener('click', () => {
+    mainPage.classList.toggle('blur');
+    popUps[i].classList.toggle('active');
+  });
+}
+
+for (let i = 0; i < closeModals.length; i += 1) {
+  closeModals[i].onclick = () => {
+    mainPage.classList.toggle('blur');
+    popUps[i].classList.toggle('active');
+  };
+}
+
+// Form validation
+
+const form = document.querySelector('.contact_form');
+const email = document.getElementById('email');
+
+function isEmail(email) {
+  return email.match(/^[_a-z\d*+.#$%^&!-]+@[a-z\d-]+(\.[a-z\d-]+)*(\.[a-z]{2,4})$/);
+}
+
+function checkInputs() {
+  const emailValue = email.value.trim();
+  function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form_controler error';
+    small.innerText = message;
+  }
+
+  if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Please enter the valid email address');
+  } else {
+    form.submit();
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+
+// Local Storage
+
+function storeData(nameData, emailData, messageData) {
+  const formData = {
+    nameData,
+    emailData,
+    messageData,
+  };
+
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+form.addEventListener('keyup', (e) => {
+  e.preventDefault();
+  const name = document.getElementById('full_name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('comment').value;
+  storeData(name, email, message);
+});
+
+const getData = JSON.parse(localStorage.getItem('formData'));
+
+window.onload = () => {
+  if (getData != null) {
+    document.getElementById('full_name').value = getData.nameData;
+    document.getElementById('email').value = getData.emailData;
+    document.getElementById('comment').value = getData.messageData;
+  }
+};
